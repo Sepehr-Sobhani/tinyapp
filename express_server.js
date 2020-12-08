4b02a0f944405f50e4f637134ef7845d05e44d9d
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-08 20:28:45 +0000

    adding new route and functionality to remove a specific URL plus adding delete button for each url

--- express_server.js
+++ express_server.js
@@ -56,6 +56,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
+//Deleting a URL
+app.post("/urls/:shortURL/delete", (req, res) => {
+  // console.log(req.params.shortURL);
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
