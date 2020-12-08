ea474e346790aebb4357a4b75484e0123df5405a
Sepehr-Sobhani <sobhani.sepehr@gmail.com>
2020-12-08 00:32:50 +0000

    creating a template for rendering urls and adding HTML tags and CSS/JS scripts to the template

--- express_server.js
+++ express_server.js
@@ -9,15 +9,13 @@ const urlDatabase = {
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+
+// a link for shortening a new URL
+// app.get()
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
